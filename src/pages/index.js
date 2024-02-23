0f40d4db65986ecfa5360979467582f2c67f1d9a
Dinaben <46792559+Dinaben@users.noreply.github.com>
2024-02-23 14:14:43 -0500

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            AGILITY Tutorial - 15 min ⏱️
           </Link>
         </div>
       </div>
